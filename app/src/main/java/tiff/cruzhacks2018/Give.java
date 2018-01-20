d3b438b96eb7b56cb49a7a432ed279bd91062f22
Alex Lue <alue@ucsc.edu>
2018-01-20 13:19:23 -0800

    Give and Receive Button Activity

---
+++ app/src/main/java/tiff/cruzhacks2018/Give.java
@@ -0,0 +1,15 @@
+package tiff.cruzhacks2018;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Give extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give);
+    }
+
+}
